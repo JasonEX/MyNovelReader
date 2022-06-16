7eeecc0005f24edb27ed1bd5624bdc6eca6a8903
821938089 <821938089@qq.com>
2022-06-16 11:39:27 +0800

    7.1.8 更新

--- rollup-plugin-compress.js
+++ rollup-plugin-compress.js
@@ -2,18 +2,29 @@ import { minify } from 'terser'
 import { createFilter } from '@rollup/pluginutils'
 
 export default function compress(opts = {}) {
+  const type = opts.type?.toLowerCase() || 'js'
   const filter = createFilter(opts.targets)
   const minifyOpts = {
     mangle: false,
     compress: { defaults: false }
   }
+  let transform
 
-  return {
-    name: 'compress',
-    async transform(code, id) {
+  if (type === 'js') {
+    transform = async function (code, id) {
       if (filter(id)) {
         return await minify(code, minifyOpts)
       }
     }
+  } else if (type === 'json') {
+    transform = async function (code, id) {
+      if (filter(id)) {
+        return JSON.stringify(JSON.parse(code))
+      }
+    }
+  } else {
+    throw new Error(`unknown file type ${type}. 未知的文件类型 ${type} 。`)
   }
+
+  return { name: 'compress', transform }
 }
