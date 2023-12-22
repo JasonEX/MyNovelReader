6f6f83ad898a06e4e0cff7d0987c3ae3618b387d
Horis <821938089@qq.com>
2023-12-22 22:44:23 +0800

    7.7.0 更新繁简转换表

--- src/MyNovelReader/meta.js
+++ src/MyNovelReader/meta.js
@@ -3,7 +3,7 @@
 // @name           My Novel Reader
 // @name:zh-CN     小说阅读脚本
 // @name:zh-TW     小說閱讀腳本
-// @version        7.6.9
+// @version        7.7.0
 // @namespace      https://github.com/ywzhaiqi
 // @author         ywzhaiqi
 // @contributor    Roger Au, shyangs, JixunMoe、akiba9527 及其他网友
@@ -32,6 +32,7 @@
 // @connect        *://*.qidian.com/
 // @connect        bgme.bid
 // @connect        xrzww.com
+// @connect        qidian.com
 
 // @include        *://vipreader.qidian.com/chapter/*/*
 // @include        *://www.qdmm.com/BookReader/*,*.aspx
