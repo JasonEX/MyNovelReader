0da21dc5ec7b6fbdabff69e001c862b4791b23fd
Yuqian YANG <yuqian.yang@msn.com>
2024-12-26 12:26:06 +0800

    tag: 7.9.8.6

--- src/MyNovelReader/meta.js
+++ src/MyNovelReader/meta.js
@@ -3,7 +3,7 @@
 // @name           My Novel Reader
 // @name:zh-CN     小说阅读脚本
 // @name:zh-TW     小說閱讀腳本
-// @version        7.9.8.5
+// @version        7.9.8.6
 // @namespace      https://github.com/ywzhaiqi
 // @author         ywzhaiqi
 // @contributor    Roger Au, shyangs, JixunMoe、akiba9527 及其他网友
