195f0b9c981a7b24bd733e1a57632c5c00aedc44
Yuqian <yuqian.yang@msn.com>
2026-07-13 14:56:15 +0800

    chore: release 9.3.4

--- scripts/MyNovelReader.user.js
+++ scripts/MyNovelReader.user.js
@@ -3,7 +3,7 @@
 // @name:zh-CN         小说阅读脚本
 // @name:zh-TW         小說閱讀腳本
 // @namespace          https://github.com/ywzhaiqi
-// @version            9.3.3
+// @version            9.3.4
 // @author             ywzhaiqi
 // @description        小说阅读脚本，统一阅读样式，内容去广告、修正拼音字、段落整理，自动下一页
 // @description:zh-CN  小说阅读脚本，统一阅读样式，内容去广告、修正拼音字、段落整理，自动下一页
@@ -8445,7 +8445,7 @@
 		else if (options) managerInstance.updateOptions(options);
 		return managerInstance;
 	}
-	var VERSION = "9.3.3";
+	var VERSION = "9.3.4";
 	var BUILD_DATE = "2026-07-13";
 	var SENSITIVE_QUERY_KEY = /(?:^|[_-])(?:token|auth|session|sid|key|sign|signature|ticket|password|passwd|pwd|jwt|credential|access|refresh|challenge|chl)(?:[_-]|$)|^__cf_/i;
 	function redactUrl(url) {
